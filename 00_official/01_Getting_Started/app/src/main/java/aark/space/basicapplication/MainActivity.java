34b27c0cfbc7b57355e041a60efcec0fc148cd4a
aarkwright <apollo.arkwright@gmail.com>
2018-02-15 13:45:29 +0200

    Button changes text.

--- 00_official/01_Getting_Started/app/src/main/java/aark/space/basicapplication/MainActivity.java
+++ 00_official/01_Getting_Started/app/src/main/java/aark/space/basicapplication/MainActivity.java
@@ -4,7 +4,9 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,11 +21,25 @@ public class MainActivity extends AppCompatActivity {
     }
 
     // Called when the user taps the send button
-    public void sendMessage(View view) {
+    public void sendMessage(View v) {
         Intent intent = new Intent(this, DisplayMessageActivity.class);
         EditText editText = (EditText) findViewById(R.id.editText);
         String message = editText.getText().toString();
         intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
+    }
+
+    public void sayHello(View v) {
+        final String helloWorld = getResources().getString(R.string.hello_world);
+        final TextView textViewHello = (TextView) findViewById(R.id.textViewHello);
+//        Button btnHello = (Button) findViewById(R.id.btnHello);
+
+        textViewHello.setText(helloWorld);
+//        btnHello.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                textViewHello.setText(helloWorld);
+//            }
+//        });
     }
 }
