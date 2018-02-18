ebd2967df8019b67c999a163e2979e6ec86d334d
aarkwright <apollo.arkwright@gmail.com>
2018-02-18 22:53:25 +0200

    Initial commit.

---
+++ 42_myApps/app/src/main/java/space/kn0wn/btchannelscanner/MainActivity.kt
@@ -0,0 +1,12 @@
+package space.kn0wn.btchannelscanner
+
+import android.app.Activity
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
